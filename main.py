d40a7c1dd2dc968b176ae408826a58445fe9e0f4
Lewis Miller <lewis.miller@gmail.com>
2025-12-26 22:52:42 -0600

    name collection functions implimented

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import readchar
+
+
 class Cousin:
     def __init__(
             self, kids_name: str, parents_name: str, 
@@ -10,8 +13,45 @@ class Cousin:
         self.age = age
         self.gift_for : str | None = None
 
+
+def get_name(who: str) -> str:
+    name: str = ""
+    while True:
+        name:str = str(input(f"\nEnter {who}'s Name: "))
+        if not name:
+            print("Name can't be empty.\n")
+            continue
+        elif check_spelling(f"{who}'s name", name) == True:
+            return name
+        else:
+            print(' ')
+
+
+def check_spelling(field: str, spelling: str) -> bool:
+    while True:
+            print(f"\nConfirm the {field} is {spelling}")
+            print("Is this correct? (Y/N)", end='', flush=True)
+            char = readchar.readchar().lower()
+            if char == "y":
+                return True
+            elif char == "n":
+                return False
+            else:
+                print("\n\nNeeds (Y)es or (N)o")
+
+
+def add_cousin() -> object:
+    pass
+
+
 def main():
     print("Hello from cousins-secret-santa!")
+    family = []
+    print(family)
+    kids_name = get_name("Child")
+    print(f"\n{kids_name}")
+    parents_name = get_name("Parent")
+    print(f"\n{kids_name}'s parent is {parents_name}")
 
 
 if __name__ == "__main__":
